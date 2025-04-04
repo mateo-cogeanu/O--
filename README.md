bf8fe1895a16ee82d72f9736392ed9b60efc1ded
githb.mateo@cogeanu.com <github.mateo@cogeanu.com>
2025-04-04 19:35:16 +0200

    O-- has been released

---
+++ README.md
@@ -0,0 +1,65 @@
+## O-- the simplest programming language i know
+
+Run code with
+
+```bash
+python interpreter.py FILE.omm
+```
+
+## Basics
+To print in `O--` all you need to do is
+
+```rs
+p "hello, world!"
+```
+And the output is
+
+```rs
+hello, world!
+```
+
+# Variables
+Variables are VERY easy all you need to do is
+
+```rs
+x = 1
+```
+
+Thats it!
+
+# Math
+Math is also very easy
+
+```rs
+x = 1
+p x + 1
+```
+other symbols work aswell `+,-,/`
+
+# If statement's
+these are very easy
+
+```rs
+x = 6
+i x > 5 { p "x is greater than 5" }
+```
+
+# Loops
+Loops are simple
+
+```rs
+l 3 { p "looped 3 times" }
+```
+
+`infinite` loops are not possible but there's a workaround
+
+```rs
+l 999999999999999999 { "infinite loop?" }
+```
+
+just shove alot of 9's there
+
+# EXIT
+```rs
+e
+```
---
+++ interpreter.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+import re
+import sys
+
+def lex(code):
+    return re.findall(r'"[^"]*"|\S+', code)
+
+def parse(tokens, variables):
+    if not tokens:
+        return None
+    
+    # Print
+    if tokens[0] == 'p':
+        return ('print', ' '.join(tokens[1:]))
+    
+    # Variable assignment
+    elif len(tokens) >= 3 and tokens[1] == '=':
+        return ('assign', tokens[0], ' '.join(tokens[2:]))
+    
+    # If statement
+    elif tokens[0] == 'i':
+        condition = ' '.join(tokens[1:tokens.index('{')])
+        body = ' '.join(tokens[tokens.index('{')+1:-1])
+        return ('if', condition, body)
+    
+    # Loop
+    elif tokens[0] == 'l':
+        times = int(tokens[1])
+        body = ' '.join(tokens[tokens.index('{')+1:-1])
+        return ('loop', times, body)
+    
+    # Function definition
+    elif tokens[0] == 'f':
+        name = tokens[1]
+        body = ' '.join(tokens[tokens.index('{')+1:-1])
+        return ('func', name, body)
+    
+    # Function call
+    elif tokens[0] in variables and isinstance(variables[tokens[0]], str):
+        return ('call', tokens[0])
+    
+    # Read input
+    elif tokens[0] == 'r':
+        prompt = ' '.join(tokens[1:]).strip('"')
+        return ('input', prompt)
+    
+    # Exit
+    elif tokens[0] == 'e':
+        return ('exit',)
+    
+    # List operations
+    elif tokens[0] == 'a' and len(tokens) > 1 and tokens[1] == '=':
+        items = ' '.join(tokens[2:]).strip('[]').split()
+        return ('list_create', tokens[0], items)
+    
+    # Get list item
+    elif '[' in tokens[0] and ']' in tokens[0]:
+        var = tokens[0].split('[')[0]
+        index = tokens[0].split('[')[1].split(']')[0]
+        return ('list_get', var, index)
+    
+    # Set list item
+    elif len(tokens) > 2 and tokens[1] == '=' and '[' in tokens[0]:
+        var = tokens[0].split('[')[0]
+        index = tokens[0].split('[')[1].split(']')[0]
+        return ('list_set', var, index, ' '.join(tokens[2:]))
+
+def evaluate(expr, variables):
+    try:
+        # Replace variable names with their values
+        for var in variables:
+            if isinstance(variables[var], (int, float, str)) and var in expr:
+                expr = expr.replace(var, str(variables[var]))
+        return eval(expr)
+    except:
+        return expr
+
+def interpret(code, variables):
+    lines = [line.strip() for line in code.split('\n') if line.strip()]
+    for line in lines:
+        tokens = lex(line)
+        action = parse(tokens, variables)
+        if not action:
+            continue
+            
+        # Print
+        if action[0] == 'print':
+            value = ' '.join(action[1:])
+            if '"' in value:
+                print(value.strip('"'))
+            else:
+                print(evaluate(value, variables))
+        
+        # Assign
+        elif action[0] == 'assign':
+            variables[action[1]] = evaluate(action[2], variables)
+        
+        # If statement
+        elif action[0] == 'if':
+            if evaluate(action[1], variables):
+                interpret(action[2], variables)
+        
+        # Loop
+        elif action[0] == 'loop':
+            for _ in range(action[1]):
+                interpret(action[2], variables)
+        
+        # Function definition
+        elif action[0] == 'func':
+            variables[action[1]] = action[2]
+        
+        # Function call
+        elif action[0] == 'call':
+            interpret(variables[action[1]], variables)
+        
+        # Input
+        elif action[0] == 'input':
+            variables['_'] = input(action[1] + ' ')
+        
+        # Exit
+        elif action[0] == 'exit':
+            sys.exit(0)
+        
+        # List create
+        elif action[0] == 'list_create':
+            variables[action[1]] = [evaluate(item, variables) for item in action[2]]
+        
+        # List get
+        elif action[0] == 'list_get':
+            index = evaluate(action[2], variables)
+            print(variables[action[1]][int(index)])
+        
+        # List set
+        elif action[0] == 'list_set':
+            index = evaluate(action[2], variables)
+            variables[action[1]][int(index)] = evaluate(action[3], variables)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python interpreter.py code.omm")
+        sys.exit(1)
+    with open(sys.argv[1], 'r') as file:
+        code = file.read()
+    interpret(code, {})
+
+if __name__ == "__main__":
+    main()
